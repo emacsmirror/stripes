9fa3d00759287ba44d8017cfd13a91c820c7ea63
Štěpán Němec <stepnem@gmail.com>
2020-02-29 00:42:56 +0100

    Release into the public domain (based on agreement with the original author)

--- stripes.el
+++ stripes.el
@@ -1,31 +1,15 @@
 ;;; stripes.el --- highlight alternating lines differently -*- lexical-binding: t -*-
 
-;; Copyright (C) 2003 Michael Schierl
-
 ;; Author: Michael Schierl <schierlm-public@gmx.de>
 ;;         Štěpán Němec <stepnem@gmail.com>
 ;; Maintainer: Štěpán Němec <stepnem@gmail.com>
 ;; Created: 02 October 2003
 ;; URL: https://gitlab.com/stepnem/stripes-el
 ;; Keywords: convenience faces
+;; License: public domain
 ;; Tested-with: GNU Emacs 26, 27, 28
 ;; Version: 0.3a
 ;; Package-Requires: ((emacs "24.3"))
-
-;; This file is not part of GNU Emacs.
-
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 2, or (at
-;; your option) any later version.
-
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
